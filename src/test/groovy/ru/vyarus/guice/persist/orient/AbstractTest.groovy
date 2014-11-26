5f7ca3f2b39978b2256421b78e104dc9ff9a6f73
Vyacheslav Rusakov <vyarus@gmail.com>
2014-11-26 13:06:29 +0600

    fix tests

--- src/test/groovy/ru/vyarus/guice/persist/orient/AbstractTest.groovy
+++ src/test/groovy/ru/vyarus/guice/persist/orient/AbstractTest.groovy
@@ -2,8 +2,11 @@ package ru.vyarus.guice.persist.orient
 
 import com.google.inject.persist.PersistService
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx
+import com.orientechnologies.orient.core.tx.OTransaction
 import com.orientechnologies.orient.object.db.OObjectDatabaseTx
 import ru.vyarus.guice.persist.orient.db.transaction.TransactionManager
+import ru.vyarus.guice.persist.orient.db.transaction.TxConfig
+import ru.vyarus.guice.persist.orient.db.transaction.template.SpecificTxAction
 import ru.vyarus.guice.persist.orient.db.transaction.template.SpecificTxTemplate
 import ru.vyarus.guice.persist.orient.support.Config
 import spock.lang.Specification
@@ -30,6 +33,14 @@ abstract class AbstractTest extends Specification {
     }
 
     void cleanup() {
+        template.doInTransaction(new TxConfig(OTransaction.TXTYPE.NOTX), { db ->
+            for (Class<?> entity :
+                    db.getEntityManager().getRegisteredEntities().findAll {
+                        it.package.name.startsWith("ru.vyarus.guice")
+                    }) {
+                db.getEntityManager().deregisterEntityClass(entity)
+            }
+        } as SpecificTxAction<Void, OObjectDatabaseTx>)
         persist.stop()
         new ODatabaseDocumentTx(Config.DB).open(Config.USER, Config.PASS).drop()
     }
