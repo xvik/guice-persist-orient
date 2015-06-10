aab3b08ecd9b85435a154bc8a76967954cac168b
Vyacheslav Rusakov <vyarus@gmail.com>
2015-06-10 23:44:07 +0600

    remote test for @CaseInsensitive scheme field extension

---
+++ src/test/groovy/ru/vyarus/guice/persist/orient/db/scheme/initializer/ext/field/ci/CiRemoteTest.groovy
@@ -0,0 +1,11 @@
+package ru.vyarus.guice.persist.orient.db.scheme.initializer.ext.field.ci
+
+import ru.vyarus.guice.persist.orient.util.remoteext.UseRemote
+
+/**
+ * @author Vyacheslav Rusakov 
+ * @since 10.06.2015
+ */
+@UseRemote
+class CiRemoteTest extends CiTest {
+}
