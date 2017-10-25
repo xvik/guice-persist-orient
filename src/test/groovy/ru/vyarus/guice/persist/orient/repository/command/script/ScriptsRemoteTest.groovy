bf4c2f7b02b260c2988672a99d6f2bee36ce1045
Vyacheslav Rusakov <vyarus@gmail.com>
2017-10-26 01:15:39 +0700

    remote script test

---
+++ src/test/groovy/ru/vyarus/guice/persist/orient/repository/command/script/ScriptsRemoteTest.groovy
@@ -0,0 +1,8 @@
+package ru.vyarus.guice.persist.orient.repository.command.script
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 26.10.2017
+ */
+class ScriptsRemoteTest extends ScriptsExecutionTest {
+}
